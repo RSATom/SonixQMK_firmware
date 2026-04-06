5a1ab10208e66d333117125417d002fa2fb62d29
sergey radionov <rsatom@gmail.com>
2026-04-06 11:48:39 +0700

    m10sp: M3/M8 = Shift

--- keyboards/filco/m10sp/keymaps/default/keymap.c
+++ keyboards/filco/m10sp/keymaps/default/keymap.c
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /*L1*/KC_ESC,          KC_1,     KC_2,    KC_3,         KC_4,            KC_5,             KC_6,         KC_GRAVE/*M1*/,     XXXXXXX,             XXXXXXX,            XXXXXXX, // C3
         /*R_*/XXXXXXX,         XXXXXXX,  XXXXXXX, XXXXXXX,      XXXXXXX,         XXXXXXX,          XXXXXXX,      XXXXXXX,            KC_GRAVE/*M6*/,      XXXXXXX,            XXXXXXX,
 
-        /*L2*/KC_TAB,          KC_Q,     KC_W,    KC_E,         KC_R,            KC_T,             KC_T,         XXXXXXX/*M3*/,      XXXXXXX,             XXXXXXX,            KC_LWIN, // C5
+        /*L2*/KC_TAB,          KC_Q,     KC_W,    KC_E,         KC_R,            KC_T,             KC_T,         LM_LSHIFT_LAYER/*M3*/, XXXXXXX,          XXXXXXX,            KC_LWIN, // C5
         /*R1*/KC_7,            KC_8,     KC_9,    KC_0,         KC_MINUS,        KC_EQUAL,         KC_BACKSPACE, XXXXXXX,            XXXXXXX/*M7*/,       TT(FN_LAYER)/*Fn*/, XXXXXXX,
 
         /*L3*/KC_CAPS_LOCK,    KC_A,     KC_S,    KC_D,         KC_F,            KC_G,             KC_G,         KC_LALT/*M4*/,      XXXXXXX,             XXXXXXX,            XXXXXXX, // C7
-        /*R_*/XXXXXXX,         XXXXXXX,  XXXXXXX, XXXXXXX,      XXXXXXX,         XXXXXXX,          KC_Y,         XXXXXXX,            XXXXXXX/*M8*/,       XXXXXXX,            XXXXXXX,
+        /*R_*/XXXXXXX,         XXXXXXX,  XXXXXXX, XXXXXXX,      XXXXXXX,         XXXXXXX,          KC_Y,         XXXXXXX,            LM_RSHIFT_LAYER/*M8*/, XXXXXXX,          XXXXXXX,
 
         /*L4*/LM_LSHIFT_LAYER, KC_Z,     KC_X,    KC_C,         KC_V,            KC_B,             KC_B,         KC_LCTL/*M5*/,      XXXXXXX,             XXXXXXX,            XXXXXXX, // C9
         /*R2*/KC_U,            KC_I,     KC_O,    KC_P,         KC_LEFT_BRACKET, KC_RIGHT_BRACKET, KC_BACKSLASH, XXXXXXX,            XXXXXXX,             XXXXXXX,            XXXXXXX,
